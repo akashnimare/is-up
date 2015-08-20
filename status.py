3aaa8e6c491e042192b032c8e969f1414bac5d05
Akash Nimare <svnitakash@gmail.com>
2015-08-20 22:37:10 +0530

    Update status.py
    Added condition if no arguments given

--- status.py
+++ status.py
@@ -4,8 +4,11 @@ import sys
 import requests
 import urllib
 from colorama import Fore, Back, Style
-try: 
-	urllib.urlopen('http://' + sys.argv[1]) 
-	print(Fore.GREEN + u"\u2713" +" Up")
-except:
-    print(Fore.RED + u"\u2718" +" Down")
+if len(sys.argv)==1:
+	print "Invalid domain"
+else:
+	try: 
+		urllib.urlopen('http://' + sys.argv[1]) 
+		print(Fore.GREEN + u"\u2713" +" Up")
+	except:
+    		print(Fore.RED + u"\u2718" +" Down")
